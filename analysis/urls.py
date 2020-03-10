3a76dabdb807851d156de51fcab11982a53c1944
innorev14 <tokyo3@netsgo.com>
2020-03-10 14:07:24 +0900

    feat/ remove trend, search in analysis

--- analysis/urls.py
+++ analysis/urls.py
@@ -7,7 +7,4 @@ app_name = 'analysis'
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
     path('inside/', views.InsideView.as_view(), name='inside'),
-    path('trend/', views.Trend.as_view(), name='trend'),
-    path('search/', views.Search.as_view(), name='search'),
-
 ]
